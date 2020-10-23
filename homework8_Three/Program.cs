f724480c13e80f8f9877236b94ca72a5408e7dd1
Mikhail Ankudinov <43526147+Magbusjap@users.noreply.github.com>
2020-10-24 03:36:41 +0500

    add new file for home work

---
+++ homework8_Three/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+namespace homework8_Three
+{
+    //Михаил Анкудинов  3..
+
+    //*Написать программу-преобразователь из CSV в XML-файл с информацией о студентах (6 урок).
+
+    class Program
+    {
+
+        static void Converter(string fileNameOpen, string fileNameSave)
+        {
+            string[] lines = File.ReadAllLines(fileNameOpen);
+            string[] headers = { "Имя", "Фамилия", "Уч.Заведение", "Факультет",
+                "Отделение", "Возвраст", "Курс", "Группа", "Город" };
+
+            var xml = new XElement("Students",
+               lines.Where((line, index) => index > 0).Select(line => new XElement("StudentIndo",
+                  line.Split(';').Select((column, index) => new XElement(headers[index], column)))));
+
+            xml.Save(fileNameSave);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует конвертер расширений из CSV в XML-файл." +
+                "\nКонвертируем информацию о студентах...");
+
+            Converter("..\\..\\students.csv", "..\\..\\students.xml");
+
+            Console.WriteLine("Работа программы завершена!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework8_Two/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework8_Two
+{
+    //Михаил Анкудинов
+
+    //Создайте простую форму на котором свяжите свойство Text 
+    //элемента TextBox со свойством Value элемента NumericUpDown
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Text<->Value";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = numericUpDown1.Value.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                numericUpDown1.Value = decimal.Parse(textBox1.Text);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show($"Некорректный ввод: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show($"Некорректный ввод: {ex.Message}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            //numericUpDown1.Select(0, numericUpDown1.Text.Length); 
+        }
+    }
+}
